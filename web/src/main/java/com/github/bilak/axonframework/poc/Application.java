c34edd68b39e05b0931a2177956e5e263050ae4d
bilak <bilaak@gmail.com>
2015-08-12 16:44:00 +0200

    - extended ServletInitializer to be able to deploy to standard containers and added actuator dependency

--- web/src/main/java/com/github/bilak/axonframework/poc/Application.java
+++ web/src/main/java/com/github/bilak/axonframework/poc/Application.java
@@ -3,6 +3,8 @@ package com.github.bilak.axonframework.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 
@@ -11,7 +13,12 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args   );
